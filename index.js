688b5567e8bebaafd88ac0eaccaf375015318b86
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-08-29 19:44:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+const logDriverNames = collection => {
+  collection.forEach(function(el) {
+    console.log(el.name)
+  })
+};
+
+function logDriversByHometown(drivers, location) {
+  drivers.forEach(function(driver) {
+    if (driver.hometown === location) {
+    console.log(driver.name)
+    }
+  })
+}
+
+function driversByRevenue(drivers) {
+    return [...drivers].sort(function(a,b) {
+     return a.revenue - b.revenue
+  })
+  }
+  
+function driversByName(drivers) {
+    return [...drivers].sort(function(a,b) {
+     return a.name.localeCompare(b.name)
+  })
+  }
+  
+  function totalRevenue(drivers) {
+   return drivers.map(driver => driver.revenue).reduce(function (accumulator, currentValue) {
+      return accumulator + currentValue;
+    }, 0)
+  }
+  
+    function averageRevenue(drivers) {
+    return totalRevenue(drivers)/(drivers.length)
+  }
